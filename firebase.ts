ac2855b7b727db0acd98ca75a5560bba594d6606
dongreji <156117915+dongreji@users.noreply.github.com>
2025-08-24 21:56:46 +0530

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -5,14 +5,15 @@ import { getDatabase } from 'firebase/database';
 // You can get this from the Firebase console:
 // Project settings > General > Your apps > Web app > Firebase SDK snippet > Config
 const firebaseConfig = {
-  apiKey: "AIzaSyXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-  authDomain: "your-project-id.firebaseapp.com",
-  databaseURL: "https://your-project-id-default-rtdb.firebaseio.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project-id.appspot.com",
-  messagingSenderId: "000000000000",
-  appId: "1:000000000000:web:0000000000000000000000"
+  apiKey: "AIzaSyCsFWIuTLgCmbQ7tTULIrzQvObLYzf6UPs",
+  authDomain: "sync-bo.firebaseapp.com",
+  projectId: "sync-bo",
+  storageBucket: "sync-bo.firebasestorage.app",
+  messagingSenderId: "641455814582",
+  appId: "1:641455814582:web:2c4de9567d642a41473317",
+  measurementId: "G-XQXPWB04BG"
 };
+
 
 // For this public clipboard to work, you must also set your Realtime Database security rules to be public.
 // In the Firebase Console, go to Realtime Database > Rules and set them to:
